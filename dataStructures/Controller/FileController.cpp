a7ea0e619c6a4ebb90e6d03d0b04fc597c83fca5
ConDawg <31518714+ConDawg@users.noreply.github.com>
2018-01-31 12:57:33 -0700

    imported the crime data stuffs

---
+++ dataStructures/Controller/FileController.cpp
@@ -0,0 +1,41 @@
+//
+//  FileController.cpp
+//  dataStructures
+//
+//  Created by Johnson, Connor on 1/31/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "FileController.hpp"
+
+using namespace std;
+
+vector<CrimeData> FileController :: readCrimeDataToVector(string filename)
+{
+    std :: vector<CrimeData> crimeVector;
+    string currentCSVLine;
+    int rowCount = 0;
+    
+    ifstream dataFile(filename);
+    
+    if (dataFile.is_open())
+    {
+        while (!dataFile.eof())
+        {
+            getline(dataFile, currentCSVLine, '\r');
+            if (rowCount != 0)
+            {
+                CrimeData row(currentCSVLine);
+                crimeVector.push_back(row);
+            }
+            rowCount++;
+        }
+        dataFile.close();
+    }
+    else
+    {
+        cerr << "NO FILE" << endl;
+    }
+    
+    return crimeVector;
+}
---
+++ dataStructures/Controller/FileController.hpp
@@ -0,0 +1,28 @@
+//
+//  FileController.hpp
+//  dataStructures
+//
+//  Created by Johnson, Connor on 1/31/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef FileController_hpp
+#define FileController_hpp
+
+#include "../Data/CrimeData.hpp"
+#include <stdio.h>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+class FileController
+{
+public:
+    static vector<CrimeData> readCrimeDataToVector(string filename);
+};
+
+#endif /* FileController_hpp */
