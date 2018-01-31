35e89d03fc42f983d313fe225712926cf5a30890
ConDawg <31518714+ConDawg@users.noreply.github.com>
2018-01-31 11:40:22 -0700

    Initial commit, created my controller folder and files and runner file

---
+++ dataStructures/Controller/Controller.cpp
@@ -0,0 +1,17 @@
+//
+//  Controller.cpp
+//  dataStructures
+//
+//  Created by Johnson, Connor on 1/31/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "Controller.hpp"
+
+using namespace std;
+
+void Controller :: start()
+{
+    
+    
+}
---
+++ dataStructures/Controller/Controller.hpp
@@ -0,0 +1,21 @@
+//
+//  Controller.hpp
+//  dataStructures
+//
+//  Created by Johnson, Connor on 1/31/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef Controller_hpp
+#define Controller_hpp
+
+#include <stdio.h>
+#include <vector>
+
+class Controller
+{
+public:
+    void start()
+    
+};
+#endif /* Controller_hpp*/
