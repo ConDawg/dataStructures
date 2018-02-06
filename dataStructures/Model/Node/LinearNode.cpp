f373e74663f3bcddae73c1e1634cd522f1c5a95f
ConDawg <31518714+ConDawg@users.noreply.github.com>
2018-02-06 13:38:43 -0700

    Created my LinearNode files and added all template methods

---
+++ dataStructures/Model/Node/LinearNode.cpp
@@ -0,0 +1,11 @@
+//
+//  LinearNode.cpp
+//  dataStructures
+//
+//  Created by Johnson, Connor on 2/6/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "LinearNode.hpp"
+
+
---
+++ dataStructures/Model/Node/LinearNode.hpp
@@ -0,0 +1,58 @@
+//
+//  LinearNode.hpp
+//  dataStructures
+//
+//  Created by Johnson, Connor on 2/6/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef LinearNode_hpp
+#define LinearNode_hpp
+
+#include "Node.hpp"
+
+template<class Type>
+class LinearNode : public Node<Type>
+{
+protected:
+    LinearNode<Type> * next;
+    
+public:
+    LinearNode();
+    LinearNode(Type Data);
+    LinearNode(Type Data, LinearNode<Type> * next);
+    void setNextNode(LinearNode<Type> * next);
+    LinearNode<Type> * getNextNode();
+};
+
+template <class Type>
+LinearNode<Type> :: LinearNode() : Node<Type>()
+{
+    this->next = nullptr;
+}
+
+template <class Type>
+LinearNode<Type> :: LinearNode(Type data) : Node<Type>()
+{
+    this->next = nullptr;
+}
+
+template <class Type>
+LinearNode<Type> :: LinearNode(Type data) : Node<Type>(data)
+{
+    this->next = next;
+}
+
+template <class Type>
+void LinearNode<Type> :: setNextNode(LinearNode<Type> * nextNodePointer )
+{
+    this->next = nextNodePointer;
+}
+
+template <class Type>
+LinearNode<Type> * LinearNode<Type> :: getNextNode()
+{
+    return next;
+}
+
+#endif /* LinearNode_hpp */
